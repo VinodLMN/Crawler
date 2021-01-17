bfadf170686f781729882d298267b0b230df3f88
VinodLMN <58941406+VinodLMN@users.noreply.github.com>
2021-01-17 20:12:32 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+
 import pandas
 import unittest
 from unittest.mock import patch
@@ -6,11 +7,30 @@ from steeleye_assmnt import ESMRData
 
 class TestRuns(unittest.TestCase):
 
+    @patch('steeleye_assmnt.BytesIO', autospec=True)
+    @patch('steeleye_assmnt.ZipFile', autospec=True)
     @patch('steeleye_assmnt.logger', autospec=True)
-    def test_get_dltins_link(self, mock_logger):
+    def test_get_dltins_link(self, mock_logger, mock_zipfile, mock_bytesio):
         """
         success scenario for get_dltins_link() method
         """
         esmr = ESMRData()
         ESMRData.get_files_load(esmr)
         self.assertTrue(mock_logger.info.called, 'logger.info() not called')
+        self.assertTrue(mock_zipfile.called, 'mock_zipfile() not called')
+        self.assertFalse(mock_bytesio.called, 'Unexpected call to BytesIO()')
+
+    @patch('steeleye_assmnt.et', autospec=True)
+    @patch('steeleye_assmnt.logger', autospec=True)
+    def test_parse_files(self, mock_logger, mock_etree):
+        """
+        success scenario for get_dltins_link() method
+        """
+        esmr = ESMRData()
+        esmr.parse_files('dummy', 'dummy')
+        self.assertTrue(mock_logger.info.called, 'logger.info() not called')
+        self.assertTrue(mock_etree.iterparse.called, 'etree.iterparse() not called')
+
+
+if __name__ == '__main__':
+    unittest.main()
